27fdacf32ba183957339939747d3a71e34c5fe15
aroderg <anothersoul1staken@gmail.com>
2025-10-08 00:50:00 +0300

    reset, transfer, print, equalize methods added

--- resource.lua
+++ resource.lua
@@ -1,12 +1,12 @@
 local Resource = {}
 Resource.__index = Resource
 
-function Resource.new(name, amount)
-    return setmetatable({name = name or "Resource", amount = amount or 0}, Resource)
+function Resource.new(name, amount, maxAmount)
+    return setmetatable({name = name or "Resource", amount = amount or 0, maxAmount = maxAmount or math.huge}, Resource)
 end
 
 function Resource:add(value)
-    self.amount = self.amount + value
+    self.amount = math.min(self.amount + value, self.maxAmount)
 end
 
 function Resource:spend(value)
@@ -16,6 +16,27 @@ function Resource:spend(value)
     else
         return false
     end
+end
+
+function Resource:reset()
+    self.amount = 0
+end
+
+function Resource:transfer(recipient, amount)
+    if self:spend(amount) then
+        recipient:add(amount)
+        return true
+    else
+        return false
+    end
+end
+
+function Resource:__print()
+    return string.format("Current %s: %d", self.name, self.amount)
+end
+
+function Resource:equalize(to)
+    self.amount = to.amount
 end
 
 return Resource
