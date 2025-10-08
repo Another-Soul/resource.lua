9daf2788d31fbacc9f2d5676771c731ca89dce20
aroderg <anothersoul1staken@gmail.com>
2025-10-08 23:26:46 +0300

    added setMax, setAmount, getPercent

--- resource.lua
+++ resource.lua
@@ -2,7 +2,7 @@ local Resource = {}
 Resource.__index = Resource
 
 function Resource.new(name, amount, maxAmount)
-    return setmetatable({name = name or "Resource", amount = amount or 0, maxAmount = maxAmount or math.huge}, Resource)
+    return setmetatable({name = name or "Resource", amount = math.min(amount, maxAmount or math.huge) or 0, maxAmount = maxAmount or math.huge}, Resource)
 end
 
 function Resource:add(value)
@@ -32,11 +32,27 @@ function Resource:transfer(recipient, amount)
 end
 
 function Resource:__print()
-    return string.format("Current %s: %d", self.name, self.amount)
+    return string.format("Current %s: %d / %s", self.name, self.amount, self.maxAmount)
 end
 
 function Resource:equalize(to)
     self.amount = to.amount
+end
+
+function Resource:setMax(amount)
+    self.maxAmount = amount
+end
+
+function Resource:setAmount(amount)
+    self.amount = math.min(amount, self.maxAmount)
+end
+
+function Resource:getPercent()
+    if self.maxAmount == math.huge or self.amount == 0 then
+        return 0
+    else
+        return self.amount / self.maxAmount
+    end
 end
 
 return Resource
